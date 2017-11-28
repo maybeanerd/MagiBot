2bbc81108c23f5bc1d925f307d01ca71eb692271
Sebastian Di Luzio <sebidiluzio@gmail.com>
2017-11-28 12:04:45 +0100

    changing how set game and dc work
    every admin can now change the status of the bot
    only the owner is allowed to shut it down from now on

--- commands/@dc.js
+++ commands/@dc.js
@@ -1,8 +1,13 @@
 module.exports = {
     main: (bot, msg) => {
-        msg.channel.send("Leaving...", "success");
+    //only Owner should be able to shut it down
+    if (msg.author.id == bot.OWNERID) {
+        msg.channel.send("Shutting down...", "success");
         setTimeout(function () {
             process.exit();
+        }else{
+        msg.channel.send("Nur der Besitzer des Bots, <@"+bot.OWNERID+"> darf mich ausschalten!", "error");
+        }
         }, 2000);
     },
     hide: true
--- commands/@setgame.js
+++ commands/@setgame.js
@@ -1,11 +1,7 @@
 module.exports = {
     main: (bot, msg) => {
-        if (msg.author.id == bot.OWNERID) {
             bot.user.setGame(msg.toString());
             bot.sendNotification("Status wurde auf \"" + msg + "\" gesetzt.", "success", msg);
-        } else {
-            bot.sendNotification("Du hast nicht die Berechtigung diesen Befehl zu verwenden.", "error", msg);
-        }
     },
     hide: true
 };
