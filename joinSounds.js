6d54f87855344e71c04b555b8946feb01dd4a7d4
Sebastian Di Luzio <sebidiluzio@gmail.com>
2017-11-30 22:29:46 +0100

    tring tim nr2

--- joinSounds.js
+++ joinSounds.js
@@ -1,6 +1,6 @@
 var sounds = {
     "185865492576075776": /*"https://canary.discordapp.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3"*/"https://www.myinstants.com/media/sounds/omae-wa-mou-shindeiru.mp3", //ich
-    "226383324262825985": "https://cdn.discordapp.com/attachments/227833391368634369/385904124442705923/Stalker_Bandit_quotes-nSOD2QDbOkc_mp3cut.net.mp3", //tim
+    "226383324262825985": /*"https://cdn.discordapp.com/attachments/227833391368634369/385904124442705923/Stalker_Bandit_quotes-nSOD2QDbOkc_mp3cut.net.mp3"*/"https://cdn.discordapp.com/attachments/227833391368634369/385904858311557121/BattoSound.mp3", //tim
     "185845248163840002": "https://www.myinstants.com/media/sounds/nanimp3.mp3", //nico
     "201768238890221568": "https://www.myinstants.com/media/sounds/ha-gay.mp3", //Kai
     "166649033669083136": "https://www.myinstants.com/media/sounds/falconpunch.swf.mp3", //matti
