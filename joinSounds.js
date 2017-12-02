074f6a1c667c9613ca0e53c3f5e9aa8f85f6d02f
Sebastian Di Luzio <sebidiluzio@gmail.com>
2017-12-03 00:54:41 +0100

    sound changes

--- joinSounds.js
+++ joinSounds.js
@@ -1,7 +1,7 @@
 var sounds = {
-    "185865492576075776": /*"https://canary.discordapp.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3"*/"https://www.myinstants.com/media/sounds/nanimp3.mp3", //ich
-    "226383324262825985": "https://cdn.discordapp.com/attachments/227833391368634369/385906076568256512/Oriam_nachrim.mp3"/*"https://cdn.discordapp.com/attachments/227833391368634369/385904858311557121/BattoSound.mp3"*/, //tim
-    "185845248163840002": "https://cdn.discordapp.com/attachments/185868764137390080/385905030533873664/ahmed-the-dead-terrorist-silence-i-kill-you_.mp3", //nico
+    "185865492576075776": "https://www.myinstants.com/media/sounds/onward.mp3", //ich
+    "226383324262825985": "https://cdn.discordapp.com/attachments/227833391368634369/386665940181450772/Stalker_-_Bandit.mp3", //tim
+    "185845248163840002": "https://cdn.discordapp.com/attachments/185868764137390080/386666373994250241/trumpetmom.mp3", //nico
     "201768238890221568": "https://www.myinstants.com/media/sounds/ha-gay.mp3", //Kai
     "166649033669083136": "https://www.myinstants.com/media/sounds/falconpunch.swf.mp3", //matti
     "237918762790027264": "https://cdn.discordapp.com/attachments/227833391368634369/385176456524857353/tim_warum_bin_ich_so_autistisch.wav", //stefan
