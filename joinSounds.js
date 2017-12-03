fa861d2269e3a21c59206497ddb0a3d688413602
Sebastian Di Luzio <sebidiluzio@gmail.com>
2017-12-03 17:16:43 +0100

    matti sound

--- joinSounds.js
+++ joinSounds.js
@@ -3,7 +3,7 @@ var sounds = {
     "226383324262825985": "https://cdn.discordapp.com/attachments/227833391368634369/386665940181450772/Stalker_-_Bandit.mp3", //tim
     "185845248163840002": "https://cdn.discordapp.com/attachments/185868764137390080/386666373994250241/trumpetmom.mp3", //nico
     "201768238890221568": "https://www.myinstants.com/media/sounds/ha-gay.mp3", //Kai
-    "166649033669083136": "https://www.myinstants.com/media/sounds/falconpunch.swf.mp3", //matti
+    "166649033669083136": "https://cdn.discordapp.com/attachments/267743281461329920/386913511722450955/Tschuldigung.mp3", //matti
     "237918762790027264": "https://cdn.discordapp.com/attachments/227833391368634369/385176456524857353/tim_warum_bin_ich_so_autistisch.wav", //stefan
     "209642947287711744": "https://cdn.discordapp.com/attachments/209646040675123200/385177764678074379/flavio_mweeeeeh.wav", //flavio
     "186052540729655296": "https://cdn.discordapp.com/attachments/186064880556703744/385181918314102794/dadadadadaaaa.wav", //severin
