f2e8126ad8c08c76525b8b0ce4e87ab0d3f4182e
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-01-06 13:01:23 +0100

    + sounds

--- joinSounds.js
+++ joinSounds.js
@@ -3,13 +3,13 @@ var sounds = {
     "226383324262825985": "https://cdn.discordapp.com/attachments/227833391368634369/386665940181450772/Stalker_-_Bandit.mp3", //tim
     "185845248163840002": "https://cdn.discordapp.com/attachments/185868764137390080/396055236482957323/Angelaaa.mp3", //nico
     "201768238890221568": "https://www.myinstants.com/media/sounds/ha-gay.mp3", //Kai
-    "166649033669083136": "https://cdn.discordapp.com/attachments/267743281461329920/386988382770692108/Fleiig_am_pimpen.mp3", //matti
+    "166649033669083136": "https://cdn.discordapp.com/attachments/267743281461329920/386985806402682881/Tschuldigung_lauter.mp3", //matti
     "237918762790027264": "https://cdn.discordapp.com/attachments/239520611389538304/390999323879669761/tagliches_mantra_kayle.mp3", //stefan
     "209642947287711744": "https://cdn.discordapp.com/attachments/209646040675123200/385177764678074379/flavio_mweeeeeh.wav", //flavio
     "186052540729655296": "https://cdn.discordapp.com/attachments/186064880556703744/393105851423916033/Mine_Is_An_Evil_Laugh.mp3", //severin
     "216979437865009152": "https://cdn.discordapp.com/attachments/229604639069175809/385902962737938432/NiksDa_-_Leb_deinen_Champ_Digimon_LoL_Parodie.mp3", //Hannes
     "165854980262199297": "https://www.myinstants.com/media/sounds/im-really-feeling-it.mp3", //Tobi
-    "295899170454372352": "https://cdn.discordapp.com/attachments/301696653877051393/397389721820594184/TBD_sfx_summonerspell_choose_flash_01.mp3", //Lucas Ashura
+    "295899170454372352": "https://cdn.discordapp.com/attachments/301696653877051393/398623143025049600/Crylikeabitch.mp3", //Lucas Ashura
     "186481395416170496": "https://cdn.discordapp.com/attachments/386915523524558849/395578990938685440/tschuldigung_mama.mp3", //Julian
     "220957687683219458": "https://cdn.discordapp.com/attachments/354273411935240192/388034541945618438/MLG_SOUND_EFFECT-_AIR_HORNS.mp3", //Rizzers
     "339901226185392131": "https://cdn.discordapp.com/attachments/388068452314775553/388068621873577985/Hitler_NEIN_1.mp3", //tauri
