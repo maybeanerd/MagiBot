917f269e6b2f2d173cc2a00d284305f30016b35f
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-01-14 00:14:24 +0100

    +sounds

--- joinSounds.js
+++ joinSounds.js
@@ -1,7 +1,7 @@
 var sounds = {
     "185865492576075776": "https://cdn.discordapp.com/attachments/185868764137390080/387360631671488513/onward.mp3", //ich
     "226383324262825985": "https://cdn.discordapp.com/attachments/227833391368634369/386665940181450772/Stalker_-_Bandit.mp3", //tim
-    "185845248163840002": "https://cdn.discordapp.com/attachments/185868764137390080/401463368843788290/fetti.mp3", //nico
+    "185845248163840002": "https://cdn.discordapp.com/attachments/185868764137390080/401830319025815553/sexykaiba.mp3", //nico
     "201768238890221568": "https://cdn.discordapp.com/attachments/212909971224920065/400688108158451712/Join_Sound.wav", //Kai
     "166649033669083136": "https://cdn.discordapp.com/attachments/267743281461329920/401450357730836480/Choo_Choo.mp3", //matti
     "237918762790027264": "https://cdn.discordapp.com/attachments/239520611389538304/390999323879669761/tagliches_mantra_kayle.mp3", //stefan
@@ -19,7 +19,8 @@ var sounds = {
     "391927619701964805": "https://cdn.discordapp.com/attachments/386915523524558849/395578994109710346/DDDDEPORTED.mp3", //filip
     "204331591705690114": "https://cdn.discordapp.com/attachments/185868764137390080/399335035997782026/Du_Schwamm.mp3", //johann
     "203247775209619456": "https://cdn.discordapp.com/attachments/203248018231787520/401465484676956161/Hearthstone_-_Murloc.mp3", //paul
-    "364416438305292291": "https://cdn.discordapp.com/attachments/399617351701758002/401453955336372234/DA_DA_DA_DA_online-audio-converter.com_1.mp3" //adrian
+    "364416438305292291": "https://cdn.discordapp.com/attachments/399617351701758002/401453955336372234/DA_DA_DA_DA_online-audio-converter.com_1.mp3", //adrian
+    "358707393623752705": "https://cdn.discordapp.com/attachments/381197362049056768/401842015371001868/Sion_Op_Guide__Atomare_Power_mp3cut.net.mp3" //Calvin
 }
 
 module.exports = {
