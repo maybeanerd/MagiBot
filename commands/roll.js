4711f401f469c313380b15f473b17c3bef7beaec
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-01-30 12:21:16 +0100

    took function that can actually be read

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,20 @@
-﻿//definition of calculation of dice, use p(input) to use
-function c(s,m,n,f,a){m=parseInt(m);if(isNaN(m))m=1;n=parseInt(n);if(isNaN(n))n=1;f=parseInt(f);a=typeof(a)=='string'?parseInt(a.replace(/\s/g,'')):0;if(isNaN(a))a=0;var r=0;for(var i=0;i<n;i++)r+=Math.floor(Math.random()*f);return r*m+a;};function p(d){return c.apply(this,d.match(/(?:(\d+)\s*\*\s*)?(\d*)d(\d+)(?:\s*([\+\-]\s*\d+))?/i));}
-
+﻿//definition of calculation of dice, use parse(input)
+function comp (s, m, n, f, a) {
+    m = parseInt( m );
+    if( isNaN( m ) ) m = 1;
+    n = parseInt( n );
+    if( isNaN( n ) ) n = 1;
+    f = parseInt( f );
+    a = typeof(a) == 'string' ? parseInt( a.replace(/\s/g, '') ) : 0;
+    if( isNaN( a ) ) a = 0;
+    var r = 0;
+    for( var i=0; i<n; i++ )
+        r += Math.floor( Math.random() * f );
+    return r * m + a;
+};
+function parse( de ) {
+    return comp.apply( this, de.match(/(?:(\d+)\s*\*\s*)?(\d*)d(\d+)(?:\s*([\+\-]\s*\d+))?/i) );
+}
 
 module.exports = {
     main: function (bot, msg) {
