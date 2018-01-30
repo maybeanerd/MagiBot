a585e72e560fefdfff585c6c4ed156daed5390f3
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-01-30 12:14:27 +0100

    added roll command
    not done yet

---
+++ commands/roll.js
@@ -0,0 +1,14 @@
+﻿//definition of calculation of dice, use p(input) to use
+function c(s,m,n,f,a){m=parseInt(m);if(isNaN(m))m=1;n=parseInt(n);if(isNaN(n))n=1;f=parseInt(f);a=typeof(a)=='string'?parseInt(a.replace(/\s/g,'')):0;if(isNaN(a))a=0;var r=0;for(var i=0;i<n;i++)r+=Math.floor(Math.random()*f);return r*m+a;};function p(d){return c.apply(this,d.match(/(?:(\d+)\s*\*\s*)?(\d*)d(\d+)(?:\s*([\+\-]\s*\d+))?/i));}
+
+
+module.exports = {
+    main: function (bot, msg) {
+        
+msg.channel.send("Für Hilfe nutze "+bot.PREFIX+"!help roll.");
+    },
+    help: 'Werfe Würfel',
+ehelp:function(bot,msg){msg.channel.send("Lasse botnameTODO für dich würfeln. Dabei kannst du 3d6 + 12, 4*d12 + 3, d100 verwenden");
+},
+    admin: false,
+};
