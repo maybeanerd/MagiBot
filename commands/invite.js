fb3ecd3ea03d1f53e395675988c7a56607182d8d
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-03-27 23:04:44 +0200

    k!invite change to create invite link functionality

--- commands/invite.js
+++ commands/invite.js
@@ -1,8 +1,11 @@
 module.exports = {
-    main: function (bot, msg) {
-        msg.channel.send("Invite me to your guild: https://goo.gl/G1mtTB");
+    main: async function (bot, msg) {
+        //TODO check if invite activated on server
+        //TODO let user define invite length
+        let invite = await msg.channel.createInvite({}, "member used invite command");
+        msg.channel.send("Here's an invite link to this channel: " + invite);
     },
-    help: "Invitelink to invite MagiBot to your own guild",
-    perm: "SEND_MESSAGES",
+    help: "create an invite link to the server",
+    perm: ["SEND_MESSAGES", "CREATE_INSTANT_INVITE"],
     admin: false
 };
