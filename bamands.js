61b661127fe2d9d3cc97600bf7b6e30e50c6a883
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-04-25 12:41:35 +0200

    added findUser construct which can also use strings not only ids

--- bamands.js
+++ bamands.js
@@ -1,3 +1,23 @@
 // commands made by Basti for use of the Bot 
 
 //TODO question-answer- procedure
+
+module.exports = {
+    findMember: async function f(guild, mention) {
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            id = mention.substr(2).slice(0, -1);
+            if (id.startsWith('!')) {
+                id = id.substr(1);
+            }
+            return id;
+        } else {
+            let member = await guild.members.find("nickname", mention).catch();
+            if (member) {
+                return member.id;
+            } else {
+                return false;
+            }
+        }
+    }
+
+};
