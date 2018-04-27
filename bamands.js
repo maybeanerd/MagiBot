e9d8339518fbeac69eb1f36aa97da820bdebc867
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-04-27 16:19:11 +0200

    added getrole to bamand constructs

--- bamands.js
+++ bamands.js
@@ -3,7 +3,7 @@
 //TODO question-answer- procedure
 
 module.exports = {
-    findMember: async function f(guild, mention,important) {
+    findMember: async function f(guild, mention, important) {
         if (mention.startsWith('<@') && mention.endsWith('>')) {
             id = mention.substr(2).slice(0, -1);
             if (id.startsWith('!')) {
@@ -12,15 +12,35 @@ module.exports = {
             return id;
         } else {
             let user = await guild.fetchMember(mention).catch();
-            if(user){
-return user.id;
-}
-if(important){
-return false;
-}
+            if (user) {
+                return user.id;
+            }
+            if (important) {
+                return false;
+            }
             let member = await guild.members.find("nickname", mention).catch();
             if (member) {
                 return member.id;
+            } else {
+                return false;
+            }
+        }
+    },
+    findRole: async function f(guild, mention, important) {
+        if (mention.startsWith('<@&') && mention.endsWith('>')) {
+            id = mention.substr(3).slice(0, -1);
+            return id;
+        } else {
+            let role = await guild.roles.get(mention).catch();
+            if (role) {
+                return role.id;
+            }
+            if (important) {
+                return false;
+            }
+            let nrole = await guild.roles.find("name", mention).catch();
+            if (nrole) {
+                return nrole.id;
             } else {
                 return false;
             }
