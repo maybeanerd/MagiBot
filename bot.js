73bfc5f215ac4ee4cc80b3071fb369390ddc8090
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-05-15 20:44:27 +0200

    changed to 2 sec

--- bot.js
+++ bot.js
@@ -281,10 +281,10 @@ var checkCommand = async function (msg, isMention) {
             if (pre == ':' || await data.commandAllowed(msg.guild.id, msg.channel.id)) {
                 let perms = commands[command].perm;
                 if (!perms || await msg.channel.permissionsFor(msg.guild.me).has(perms)) {
-                    //4 sec cooldown for command usage
-                    if (!userCooldowns[msg.author.id] || userCooldowns[msg.author.id] > new Date()) {
+                    //2 sec cooldown for command usage
+                    if (!userCooldowns[msg.author.id] || userCooldowns[msg.author.id] < new Date()) {
                         var dt = new Date();
-                        dt.setSeconds(dt.getSeconds() + 4);
+                        dt.setSeconds(dt.getSeconds() + 2);
                         userCooldowns[msg.author.id] = dt;
                         commands[command].main(bot, msg);
                     } else {
