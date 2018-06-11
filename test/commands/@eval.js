0e6a21d36409948f7e474d54ab1df1c7f5a4e1f9
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-06-11 15:21:15 +0200

    formatted

--- test/commands/@eval.js
+++ test/commands/@eval.js
@@ -1,13 +1,13 @@
 ﻿
 module.exports = {
     main: function (bot, msg) {
-       var expression = new String(msg.content);
-msg.channel.send("```js"+eval(expression.toString())+"```");
+        var expression = new String(msg.content);
+        msg.channel.send("```js" + eval(expression.toString()) + "```");
     },
     help: 'eval something',
     admin: true,
-    dev:true,
-    hide: false,
+    dev: true,
+    hide: true,
     perm: ["SEND_MESSAGES", "MANAGE_MESSAGES"],
     category: "Admin Command"
 };
