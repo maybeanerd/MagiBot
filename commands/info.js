02d796fffc8473dbd165c49b47c6dd087e75a453
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-06-23 16:31:06 +0200

    changed info
    - added uptime info when used by owner
    - created strike command blueprint, still needs functionality and backend

--- commands/info.js
+++ commands/info.js
@@ -12,40 +12,48 @@ module.exports = {
             value: bot.users.size,
             inline: false
         });
-        /*
-        //uptime calc
-        var u = bot.uptime;
-        var uptime = "";
-        //days
-        var x = Math.floor(u / 3600000 / 24);
-        if (x > 0) {
-            uptime += x + "d : ";
+
+        if (msg.user.id == bot.OWNERID) {
+            //uptime calc
+            var u = bot.uptime;
+            var uptime = "";
+            //days
+            var x = Math.floor(u / 3600000 / 24);
+            if (x > 0) {
+                uptime += x + "d : ";
+            }
+            //hours
+            x = Math.floor(u / 3600000) % 24;
+            if (x > 0) {
+                uptime += x + "h : ";
+            }
+            //mins
+            x = Math.floor(u / 60000) % 60;
+            if (x > 0) {
+                uptime += x + "m : ";
+            }
+            //secs
+            x = Math.floor(u / 1000) % 60;
+            if (x > 0) {
+                uptime += x + "s";
+            }
+            //enbettung in ausgabe
+            info.push({
+                name: "Uptime",
+                value: uptime,
+                inline: false
+            });
         }
-        //hours
-        x = Math.floor(u / 3600000) % 24;
-        if (x > 0) {
-            uptime += x + "h : ";
-        }
-        //mins
-        x = Math.floor(u / 60000) % 60;
-        if (x > 0) {
-            uptime += x + "m : ";
-        }
-        //secs
-        x = Math.floor(u / 1000) % 60;
-        if (x > 0) {
-            uptime += x + "s";
-        }
-        //enbettung in ausgabe
+
         info.push({
-            name: "Uptime",
-            value: uptime,
+            name: "Support MagiBot",
+            value: "Vote on [discordbots.org](https://discordbots.org/bot/384820232583249921) and [bots.ondiscord.xyz](https://bots.ondiscord.xyz/bots/384820232583249921)!\n(vote reminder via " + bot.PREFIXES[msg.guild.id] + ".dbl)\nSupport me on [Patreon](https://www.patreon.com/MagiBot)",
             inline: false
         });
-        */
+
         info.push({
             name: "Links",
-            value: "Support me by voting on [discordbots.org](https://discordbots.org/bot/384820232583249921) and [bots.ondiscord.xyz](https://bots.ondiscord.xyz/bots/384820232583249921)!\n(vote reminder via `" + bot.PREFIXES[msg.guild.id] + ".dbl`)\n[Invite me to your guild](https://discordapp.com/oauth2/authorize?client_id=384820232583249921&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2F2Evcf4T&scope=bot)\n[Official support Discord](https://discord.gg/2Evcf4T)\n[Support me on Patreon](https://www.patreon.com/MagiBot)",
+            value: "[Invite me to your guild](https://discordapp.com/oauth2/authorize?client_id=384820232583249921&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2F2Evcf4T&scope=bot)\n[Official support Discord](https://discord.gg/2Evcf4T)",
             inline: false
         });
 
