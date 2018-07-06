27e769b7956870ba34c1a898ec9f1fd04bcffdfe
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-07-06 14:50:21 +0200

    gave TeaPot some more functions

---
+++ TeaPot/commands/@dc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    main: (bot, msg) => {
+        msg.channel.send("Shutting down...", "success");
+        setTimeout(function () {
+            process.exit();
+        }, 2000);
+
+
+    },
+    admin: true,
+    perm: ["SEND_MESSAGES", "MANAGE_GUILD"],
+    hide: true,
+    dev: true,
+    category: "Utility"
+};
