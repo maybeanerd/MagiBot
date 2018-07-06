81b9252712aa127d2db3e762467aac115ac6a54e
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-07-06 13:31:21 +0200

    added a structure to fix mutes on disconnect

--- test/bot.js
+++ test/bot.js
@@ -449,12 +449,20 @@ bot.on('error', (err) => {
 bot.on("voiceStateUpdate", async function (o, n) {
     //check if voice channel actually changed
     if ((!o.voiceChannel || o.voiceChannelID != n.voiceChannelID)) {
+        if (n.serverMute && await data.isStillMuted(n.id, n.guild.id)) {
+            n.setMute(false, "was still muted from a queue which user disconnected from");
+            data.toggleStillMuted(n.id, n.guild.id, false);
+        }
         if (bot.queueVoiceChannels[n.guild.id] && bot.queueVoiceChannels[n.guild.id] == n.voiceChannelID) {
             //user joined a muted channel
             n.setMute(true, "joined active queue voice channel");
         } else if (bot.queueVoiceChannels[o.guild.id] && bot.queueVoiceChannels[o.guild.id] == o.voiceChannelID) {
             //user left a muted channel
-            n.setMute(false, "left active queue voice channel");
+            if (n.voiceChannel) {
+                n.setMute(false, "left active queue voice channel");
+            } else {
+                data.toggleStillMuted(n.id, n.guild.id, true);
+            }
         } else if (n.voiceChannel && !(await n.guild.me.voiceChannel) && n.id != bot.user.id && !(await data.isBlacklistedUser(n.id, n.guild.id)) && await data.joinable(n.guild.id, n.voiceChannelID)) {
             if (await n.voiceChannel.permissionsFor(n.guild.me).has("CONNECT")) {
                 let sound = await data.getSound(n.id, n.guild.id);
