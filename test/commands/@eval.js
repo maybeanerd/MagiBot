b70d1e6737802f16eaa5bc72241069535f6ac1cf
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-07-12 11:40:04 +0200

    an idea for async eval

--- test/commands/@eval.js
+++ test/commands/@eval.js
@@ -1,13 +1,16 @@
 ﻿
 module.exports = {
-    main: function (bot, msg) {
+    main: async (bot, msg)=> {
         var expression = new String(msg.content);
-        msg.channel.send("```js\n" + eval(expression.toString()) + "```");
+let evaluation= await eval(expression.toString()); //is the toString needed?
+        msg.channel.send("input:\n```js\n" + expression + "```");
+
+        msg.channel.send("output:\n```js\n" + evaluation + "```");
     },
     help: 'eval something',
     admin: true,
     dev: true,
     hide: true,
-    perm: ["SEND_MESSAGES", "MANAGE_MESSAGES"],
+    perm: "SEND_MESSAGES",
     category: "Admin Command"
 };
