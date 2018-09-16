7135b1086d47789ef4401047da0641ee4caa5472
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-09-16 12:55:39 +0200

    linted teaPots commands

--- TeaPot/commands/unpour.js
+++ TeaPot/commands/unpour.js
@@ -1,14 +1,14 @@
 module.exports = {
-    main: async function (bot, msg) {
-        let mem = await msg.guild.fetchMember(msg.author);
-        mem.removeRole("460218236185739264").catch(() => { });
-        msg.reply("successfully removed the role.");
-    },
-    ehelp: function (msg, bot) {
-        return [{ name: "", value: "Get rid of your <@&460218236185739264> role." }];
-    },
-    perm: ["SEND_MESSAGES", "MANAGE_ROLES"],
-    admin: false,
-    hide: false,
-    category: "Utility"
+  async main(bot, msg) {
+    const mem = await msg.guild.fetchMember(msg.author);
+    mem.removeRole('460218236185739264').catch(() => { });
+    msg.reply('successfully removed the role.');
+  },
+  ehelp() {
+    return [{ name: '', value: 'Get rid of your <@&460218236185739264> role.' }];
+  },
+  perm: ['SEND_MESSAGES', 'MANAGE_ROLES'],
+  admin: false,
+  hide: false,
+  category: 'Utility'
 };
