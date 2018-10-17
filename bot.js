20387d1d0ed6ad0905fc4dda7c90927495d49620
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-10-17 17:12:46 +0200

    added some catches

--- bot.js
+++ bot.js
@@ -325,81 +325,85 @@ const loadCommands = function() {
 };
 
 const checkCommand = async function(msg, isMention) {
+  try {
   // ignore blacklisted users
-  if (await data.isBlacklistedUser(await msg.author.id, await msg.guild.id)) {
-    msg.delete();
-    return;
-  }
-  let command;
-  if (isMention) {
-    command = msg.content.split(' ')[1];
-    msg.content = msg.content.split(' ').splice(2, msg.content.split(' ').length).join(' ');
-    command = `.${command}`;
-  } else {
-    command = msg.content.substring(bot.PREFIXES[msg.guild.id].length, msg.content.length).split(' ')[0].toLowerCase();
-    msg.content = msg.content.slice(command.length + bot.PREFIXES[msg.guild.id].length); // delete prefix and command
-    msg.content = msg.content.replace(/^\s+/g, ''); // delete leading spaces
-  }
-  if (command) {
-    const pre = command.charAt(0);
-    switch (pre) {
-    case '.':
-      command = command.slice(1);
-      break;
-    case ':':
-      command = `@${command.slice(1)}`;
-      // Check if its an admin command, if not you're allowed to use the normal version as admin (in any channel)
-      if (!commands[command]) {
+    if (await data.isBlacklistedUser(await msg.author.id, await msg.guild.id)) {
+      msg.delete();
+      return;
+    }
+    let command;
+    if (isMention) {
+      command = msg.content.split(' ')[1];
+      msg.content = msg.content.split(' ').splice(2, msg.content.split(' ').length).join(' ');
+      command = `.${command}`;
+    } else {
+      command = msg.content.substring(bot.PREFIXES[msg.guild.id].length, msg.content.length).split(' ')[0].toLowerCase();
+      msg.content = msg.content.slice(command.length + bot.PREFIXES[msg.guild.id].length); // delete prefix and command
+      msg.content = msg.content.replace(/^\s+/g, ''); // delete leading spaces
+    }
+    if (command) {
+      const pre = command.charAt(0);
+      switch (pre) {
+      case '.':
         command = command.slice(1);
+        break;
+      case ':':
+        command = `@${command.slice(1)}`;
+        // Check if its an admin command, if not you're allowed to use the normal version as admin (in any channel)
+        if (!commands[command]) {
+          command = command.slice(1);
+        }
+        // Check if the command exists, to not just spam k: messages
+        if (commands[command]) {
+          if (!(msg.member && await data.isAdmin(msg.guild.id, msg.member, bot))) {
+            if (await msg.channel.permissionsFor(msg.guild.me).has('MANAGE_MESSAGES')) {
+              msg.delete();
+            }
+            if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
+              msg.reply("you're not allowed to use this command.").then(mess => mess.delete(5000));
+            }
+            return;
+          }
+        } else {
+          return;
+        }
+        break;
+      default:
+        return;
       }
-      // Check if the command exists, to not just spam k: messages
-      if (commands[command]) {
-        if (!(msg.member && await data.isAdmin(msg.guild.id, msg.member, bot))) {
+      if (commands[command] && (!commands[command].dev || msg.author.id == bot.OWNERID)) {
+        if (pre == ':' || await data.commandAllowed(msg.guild.id, msg.channel.id)) {
+          const perms = commands[command].perm;
+          if (!perms || await msg.channel.permissionsFor(msg.guild.me).has(perms)) {
+          // cooldown for command usage
+            if (!userCooldowns.has(msg.author.id)) {
+              userCooldowns.add(msg.author.id);
+              setTimeout(() => {
+                userCooldowns.delete(msg.author.id);
+              }, 4000);
+              try {
+                await commands[command].main(bot, msg);
+              } catch (err) {
+                bamands.catchError(err, bot, msg, `${bot.PREFIXES[msg.guild.id]}${pre}${command.slice(1)}`);
+              }
+              data.usageUp(msg.author.id, msg.guild.id);
+            } else if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
+              msg.reply("whoa cool down, you're using commands too quick!");
+            }
+          // endof cooldown management
+          } else if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
+            msg.channel.send(`I don't have the permissions needed for this command. (${perms}) `);
+          }
+        } else if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
           if (await msg.channel.permissionsFor(msg.guild.me).has('MANAGE_MESSAGES')) {
             msg.delete();
           }
-          if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
-            msg.reply("you're not allowed to use this command.").then(mess => mess.delete(5000));
-          }
-          return;
+          msg.reply(`commands aren't allowed in <#${msg.channel.id}>. Use them in ${await data.commandChannel(msg.guild.id)}. If you're an admin use \`${bot.PREFIX}:help\` to see how you can change that.`).then(mess => mess.delete(15000));
         }
-      } else {
-        return;
-      }
-      break;
-    default:
-      return;
-    }
-    if (commands[command] && (!commands[command].dev || msg.author.id == bot.OWNERID)) {
-      if (pre == ':' || await data.commandAllowed(msg.guild.id, msg.channel.id)) {
-        const perms = commands[command].perm;
-        if (!perms || await msg.channel.permissionsFor(msg.guild.me).has(perms)) {
-          // cooldown for command usage
-          if (!userCooldowns.has(msg.author.id)) {
-            userCooldowns.add(msg.author.id);
-            setTimeout(() => {
-              userCooldowns.delete(msg.author.id);
-            }, 4000);
-            try {
-              await commands[command].main(bot, msg);
-            } catch (err) {
-              bamands.catchError(err, bot, msg, `${bot.PREFIXES[msg.guild.id]}${pre}${command.slice(1)}`);
-            }
-            data.usageUp(msg.author.id, msg.guild.id);
-          } else if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
-            msg.reply("whoa cool down, you're using commands too quick!");
-          }
-          // endof cooldown management
-        } else if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
-          msg.channel.send(`I don't have the permissions needed for this command. (${perms}) `);
-        }
-      } else if (await msg.channel.permissionsFor(msg.guild.me).has('SEND_MESSAGES')) {
-        if (await msg.channel.permissionsFor(msg.guild.me).has('MANAGE_MESSAGES')) {
-          msg.delete();
-        }
-        msg.reply(`commands aren't allowed in <#${msg.channel.id}>. Use them in ${await data.commandChannel(msg.guild.id)}. If you're an admin use \`${bot.PREFIX}:help\` to see how you can change that.`).then(mess => mess.delete(15000));
       }
     }
+  } catch (err) {
+    bamands.catchErrorOnDiscord(bot, `in check command: ${err}`);
   }
 };
 
@@ -431,8 +435,12 @@ bot.on('message', msg => {
 });
 
 async function guildPrefixStartup(guild) {
-  await data.addGuild(guild.id);
-  bot.PREFIXES[guild.id] = await data.getPrefixE(guild.id);
+  try {
+    await data.addGuild(guild.id);
+    bot.PREFIXES[guild.id] = await data.getPrefixE(guild.id);
+  } catch (err) {
+    bamands.catchErrorOnDiscord(bot, `in guildPrefixStartup: ${err}`);
+  }
 }
 
 bot.on('guildCreate', guild => {
@@ -464,45 +472,49 @@ bot.on('error', err => {
 });
 
 bot.on('voiceStateUpdate', async (o, n) => {
+  try {
   // check if voice channel actually changed, don't mute bots
-  if (!n.user.bot && (!o.voiceChannel || o.voiceChannelID != n.voiceChannelID)) {
+    if (!n.user.bot && (!o.voiceChannel || o.voiceChannelID != n.voiceChannelID)) {
     // is muted and joined a vc? maybe still muted from queue
-    if (n.serverMute && n.voiceChannel && await data.isStillMuted(n.id, n.guild.id)) {
-      n.setMute(false, 'was still muted from a queue which user disconnected from');
-      data.toggleStillMuted(n.id, n.guild.id, false);
-    }
-    if (!n.serverMute && n.voiceChannel && bot.queueVoiceChannels[n.guild.id] && bot.queueVoiceChannels[n.guild.id] == n.voiceChannelID) {
-      // user joined a muted channel
-      n.setMute(true, 'joined active queue voice channel');
-    } else if (n.serverMute && o.voiceChannel && bot.queueVoiceChannels[o.guild.id] && bot.queueVoiceChannels[o.guild.id] == o.voiceChannelID) {
-      // user left a muted channel
-      if (n.voiceChannel) {
-        n.setMute(false, 'left active queue voice channel');
-      } else {
-        // save the unmute for later
-        data.toggleStillMuted(n.id, n.guild.id, true);
+      if (n.serverMute && n.voiceChannel && await data.isStillMuted(n.id, n.guild.id)) {
+        n.setMute(false, 'was still muted from a queue which user disconnected from');
+        data.toggleStillMuted(n.id, n.guild.id, false);
       }
-    } else if (n.voiceChannel && !await n.guild.me.voiceChannel && n.id != bot.user.id && !await data.isBlacklistedUser(n.id, n.guild.id) && await data.joinable(n.guild.id, n.voiceChannelID)) {
-      if (await n.voiceChannel.permissionsFor(n.guild.me).has('CONNECT')) {
-        const sound = await data.getSound(n.id, n.guild.id);
-        if (sound) {
-          n.voiceChannel.join().then(connection => {
-            if (connection) {
-            // TODO use connection.play when discord.js updates
-              const dispatcher = connection.playArbitraryInput(sound, { seek: 0, volume: 0.2, passes: 1, bitrate: 'auto' });
-              dispatcher.once('end', () => {
-                connection.disconnect();
-                dispatcher.removeAllListeners(); // To be sure noone listens to this anymore
-              });
-              dispatcher.once('error', () => {
-                connection.disconnect();
-                dispatcher.removeAllListeners(); // To be sure noone listens to this anymore
-              });
-            }
-          });
+      if (!n.serverMute && n.voiceChannel && bot.queueVoiceChannels[n.guild.id] && bot.queueVoiceChannels[n.guild.id] == n.voiceChannelID) {
+      // user joined a muted channel
+        n.setMute(true, 'joined active queue voice channel');
+      } else if (n.serverMute && o.voiceChannel && bot.queueVoiceChannels[o.guild.id] && bot.queueVoiceChannels[o.guild.id] == o.voiceChannelID) {
+      // user left a muted channel
+        if (n.voiceChannel) {
+          n.setMute(false, 'left active queue voice channel');
+        } else {
+        // save the unmute for later
+          data.toggleStillMuted(n.id, n.guild.id, true);
+        }
+      } else if (n.voiceChannel && !await n.guild.me.voiceChannel && n.id != bot.user.id && !await data.isBlacklistedUser(n.id, n.guild.id) && await data.joinable(n.guild.id, n.voiceChannelID)) {
+        if (await n.voiceChannel.permissionsFor(n.guild.me).has('CONNECT')) {
+          const sound = await data.getSound(n.id, n.guild.id);
+          if (sound) {
+            n.voiceChannel.join().then(connection => {
+              if (connection) {
+                // TODO use connection.play when discord.js updates
+                const dispatcher = connection.playArbitraryInput(sound, { seek: 0, volume: 0.2, passes: 1, bitrate: 'auto' });
+                dispatcher.once('end', () => {
+                  connection.disconnect();
+                  dispatcher.removeAllListeners(); // To be sure noone listens to this anymore
+                });
+                dispatcher.once('error', () => {
+                  connection.disconnect();
+                  dispatcher.removeAllListeners(); // To be sure noone listens to this anymore
+                });
+              }
+            });
+          }
         }
       }
     }
+  } catch (err) {
+    bamands.catchErrorOnDiscord(bot, `in voice update: ${err}`);
   }
 });
 
