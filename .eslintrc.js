9ba6b823ccd728d04002fe4b0ccf634918997237
T0TProduction <sebidiluzio@gmail.com>
2019-12-14 16:36:24 +0100

    add eslint rules, adjust package.json

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+  ],
+  rules: {
+    'import/no-unresolved': 'error', //maybe we do not need this
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-extra-semi': 0,
+    semi: 2,
+    indent: ['warn', 2],
+    quotes: [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: false,
+        avoidEscape: true,
+      },
+    ],
+    camelcase: 0, //'warn',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-plusplus': 0,
+    'class-methods-use-this': 0, // Nuxt sometimes needs functions without this , to use them in our html part
+    "no-useless-constructor": "off", // TS has some issues with this, so we use their check
+    "@typescript-eslint/no-useless-constructor": "error",
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      node: {},
+      typescript: {},
+    },
+  },
+};
