3e5f78f22d950fd996e7d3c817f9adeac4e53b3d
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-01-01 20:41:59 +0100

    clean up type errors in bamands
    still didnt clean the code up e.g. using awaits

--- src/bamands.ts
+++ src/bamands.ts
@@ -1,13 +1,15 @@
 ﻿// commands made by Basti for use of the Bot
 // we need this in here to get channels, so dependency cycle isnt a problem
+import Discord from 'discord.js';
 import { bot } from './bot'; // eslint-disable-line import/no-cycle
+import { PREFIXES } from './shared_assets';
 
 export default {
-  findMember: async function findMember(guild, mention) {
-    if (!mention) {
+  findMember: async function findMember(guild:Discord.Guild, ment:string) {
+    if (!ment) {
       return false;
     }
-    mention = mention.toLowerCase();
+    const mention = ment.toLowerCase();
     if (mention.startsWith('<@') && mention.endsWith('>')) {
       let id = mention.slice(2, -1);
       if (id.startsWith('!')) {
@@ -15,29 +17,33 @@ export default {
       }
       return id;
     }
-    const user = await guild.fetchMember(mention).catch(() => { });
+    const user = guild.member(mention);
     if (user) {
       return user.id;
     }
     if (mention.length >= 3) {
-      let memberArray = await guild.members.filterArray((memb) => memb.displayName.toLowerCase().startsWith(mention));
-      if (memberArray.length == 1) {
+      let memberArray = guild.members.filter(
+        (memb) => memb.displayName.toLowerCase().startsWith(mention),
+      );
+      if (memberArray.size === 1) {
         return memberArray[0].id;
       }
-      if (memberArray.length == 0) {
-        memberArray = await guild.members.filterArray((memb) => memb.displayName.toLowerCase().includes(mention));
-        if (memberArray.length == 1) {
+      if (memberArray.size === 0) {
+        memberArray = guild.members.filter(
+          (memb) => memb.displayName.toLowerCase().includes(mention),
+        );
+        if (memberArray.size === 1) {
           return memberArray[0].id;
         }
       }
     }
     return false;
   },
-  findRole: async function findRole(guild, mention) {
-    if (!mention) {
+  findRole: async function findRole(guild:Discord.Guild, ment:string) {
+    if (!ment) {
       return false;
     }
-    mention = mention.toLowerCase();
+    const mention = ment.toLowerCase();
     if (mention.startsWith('<@&') && mention.endsWith('>')) {
       const id = mention.substr(3).slice(0, -1);
       return id;
@@ -47,30 +53,32 @@ export default {
       return role.id;
     }
     if (mention.length >= 3) {
-      let roleArray = await guild.roles.filterArray((rol) => rol.name.toLowerCase().startsWith(mention));
-      if (roleArray.length == 1) {
+      let roleArray = await guild.roles.filter((rol) => rol.name.toLowerCase().startsWith(mention));
+      if (roleArray.size === 1) {
         return roleArray[0].id;
       }
-      if (roleArray.length == 0) {
-        roleArray = await guild.roles.filterArray((rol) => rol.name.toLowerCase().includes(mention));
-        if (roleArray.length == 1) {
+      if (roleArray.size === 0) {
+        roleArray = await guild.roles.filter((rol) => rol.name.toLowerCase().includes(mention));
+        if (roleArray.size === 1) {
           return roleArray[0].id;
         }
       }
     }
     return false;
   },
-  // this is an idea to implement rather reusable confirmation processes. ; abortMessage, timeoutMessage and time are optional parameters
-  yesOrNo: async (msg, question, abortMessage, timeoutMessage, time) => msg.channel.send(question).then(async (mess) => {
-    const filter = (reaction, user) => (reaction.emoji.name == '☑' || reaction.emoji.name == '❌') && user.id === msg.author.id;
+  // this is an idea to implement rather reusable confirmation processes.
+  // ; abortMessage, timeoutMessage and time are optional parameters
+  yesOrNo: async (msg, question, abortMessage, timeoutMessage, tim?:number) => msg.channel.send(question).then(async (mess) => {
+    const filter = (reaction, user) => (reaction.emoji.name === '☑' || reaction.emoji.name === '❌') && user.id === msg.author.id;
     await mess.react('☑');
     await mess.react('❌');
+    let time = tim;
     if (!time) {
       time = 20000;
     }
     return mess.awaitReactions(filter, { max: 1, time }).then(async (reacts) => {
       mess.delete();
-      if (reacts.first() && reacts.first().emoji.name == '☑') {
+      if (reacts.first() && reacts.first().emoji.name === '☑') {
         return true;
       } if (reacts.first()) {
         if (abortMessage) {
@@ -78,29 +86,36 @@ export default {
         }
         return false;
       }
-      if (!timeoutMessage) {
-        timeoutMessage = 'Cancelled due to timeout.';
+      let message = timeoutMessage;
+      if (!message) {
+        message = 'Cancelled due to timeout.';
       }
-      await msg.channel.send(timeoutMessage);
+      await msg.channel.send(message);
       return false;
     });
   }),
   printError: (error) => {
     console.error(`Errorstatus: ${error.response.status} ${error.response.statusText}`);
   },
-  catchError: (err, msg, command) => {
+  catchError: (error, msg, command) => {
+    let err = error;
     if (err.stack) {
       err = err.stack;
     }
     console.error(`Caught:\n${err}\nin command ${command} ${msg.content}`);
     const chann = bot.channels.get('414809410448261132');
-    chann.send(`**Command:** ${command} ${msg.content}\n**Caught Error:**\n\`\`\`${err}\`\`\``);
-    msg.reply(`something went wrong while using ${command}. The devs have been automatically notified.\nIf you can reproduce this, consider using \`${bot.PREFIXES[msg.guild.id]}.bug <bugreport>\` or join the support discord (link via \`${bot.PREFIXES[msg.guild.id]}.info\`) to tell us exactly how.`).catch(() => {});
+    if (chann) {
+      (chann as Discord.TextChannel).send(`**Command:** ${command} ${msg.content}\n**Caught Error:**\n\`\`\`${err}\`\`\``);
+    }
+    msg.reply(`something went wrong while using ${command}. The devs have been automatically notified.
+If you can reproduce this, consider using \`${PREFIXES[msg.guild.id]}.bug <bugreport>\` or join the support discord (link via \`${PREFIXES[msg.guild.id]}.info\`) to tell us exactly how.`);
   },
-  catchErrorOnDiscord: (bot, err) => {
+  catchErrorOnDiscord: (err) => {
     try {
       const chann = bot.channels.get('414809410448261132');
-      chann.send(`Caught error: ${err}`);
+      if (chann) {
+        (chann as Discord.TextChannel).send(`Caught error: ${err}`);
+      }
     } catch (error) {
       console.error(error);
     }
