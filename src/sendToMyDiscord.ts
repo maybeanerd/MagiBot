27231065c74002eb899fa35063b3092231581672
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-03-25 17:49:28 +0100

    Update sendToMyDiscord.ts

--- src/sendToMyDiscord.ts
+++ src/sendToMyDiscord.ts
@@ -3,9 +3,9 @@ import Discord from 'discord.js';
 import { bot } from './bot';
 
 
-export function catchErrorOnDiscord(err) {
+export async function catchErrorOnDiscord(err) {
   try {
-    const chann = bot.channels.get('414809410448261132');
+    const chann = await bot.channels.fetch('414809410448261132');
     if (chann) {
       (chann as Discord.TextChannel).send(`Caught error: ${err}`);
     }
