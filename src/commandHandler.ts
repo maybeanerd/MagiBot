f1ff92cb0550b65d869fd471a7b1aa9f04e6be6a
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-03-27 10:22:05 +0100

    add commands to command handler

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -5,6 +5,22 @@ import { rfact } from './commands/rfact';
 // eslint-disable-next-line import/no-cycle
 import { stats } from './commands/stats';
 import { salt } from './commands/salt';
+// eslint-disable-next-line import/no-cycle
+import { bug } from './commands/bug';
+import { inf as info } from './commands/info';
+import { inv as invite } from './commands/invite';
+import { ping } from './commands/ping';
+import { profile } from './commands/profile';
+import { roll } from './commands/roll';
+import { dc as _dc } from './commands/@dc';
+import { evall as _eval } from './commands/@eval';
+// eslint-disable-next-line import/no-cycle
+import { queue as _queue } from './commands/@queue';
+import { salt as _salt } from './commands/@salt';
+import { sound as _sound } from './commands/@sound';
+import { setup as _setup } from './commands/@setup';
+// eslint-disable-next-line import/no-cycle
+import { update as _update } from './commands/@update';
 // we allow this cycle once, as the help command also needs to list itself
 import { help } from './commands/help'; // eslint-disable-line import/no-cycle
 
@@ -13,24 +29,55 @@ import {
   PREFIXES, OWNERID, DELETE_COMMANDS, user,
 } from './shared_assets';
 
-export const commands:{[k:string]:magibotCommand} = {
-  help, salt, stats, rfact, sound, vote,
+export const commands: { [k: string]: magibotCommand } = {
+  _dc,
+  _eval,
+  _queue,
+  _sound,
+  _setup,
+  _update,
+  _salt,
+  help,
+  salt,
+  stats,
+  rfact,
+  sound,
+  vote,
+  bug,
+  info,
+  invite,
+  ping,
+  profile,
+  roll,
 };
 
-async function catchError(error:Error, msg:Discord.Message, command:string, bot:Discord.Client) {
-  console.error(`Caught:\n${error.stack}\nin command ${command} ${msg.content}`);
+async function catchError(
+  error: Error,
+  msg: Discord.Message,
+  command: string,
+  bot: Discord.Client,
+) {
+  console.error(
+    `Caught:\n${error.stack}\nin command ${command} ${msg.content}`,
+  );
   const chann = await bot.channels.fetch('414809410448261132');
   if (chann) {
-    (chann as Discord.TextChannel).send(`**Command:** ${command} ${msg.content}\n**Caught Error:**\n\`\`\`${error.stack}\`\`\``);
+    (chann as Discord.TextChannel).send(
+      `**Command:** ${command} ${msg.content}\n**Caught Error:**\n\`\`\`${error.stack}\`\`\``,
+    );
   }
   msg.reply(`something went wrong while using ${command}. The devs have been automatically notified.
-If you can reproduce this, consider using \`${PREFIXES[msg.guild ? msg.guild.id : 0]}.bug <bugreport>\` or join the support discord (link via \`${PREFIXES[msg.guild ? msg.guild.id : 0]}.info\`) to tell us exactly how.`);
+If you can reproduce this, consider using \`${
+  PREFIXES[msg.guild ? msg.guild.id : 0]
+}.bug <bugreport>\` or join the support discord (link via \`${
+  PREFIXES[msg.guild ? msg.guild.id : 0]
+}.info\`) to tell us exactly how.`);
 }
 
 const userCooldowns = new Set<string>();
 
 // TODO check why the bot also gives us Discord.PartialMessage, as thats not what we want
-export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
+export async function checkCommand(msg: Discord.Message, bot: Discord.Client) {
   if (!(msg.author && msg.guild && msg.guild.me)) {
     // check for valid message
     console.error('Invalid message received:', msg);
@@ -40,10 +87,10 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
     // check for in guild and text channel
     return;
   }
-  let isMention:boolean;
+  let isMention: boolean;
   if (
     msg.content.startsWith(`<@${user().id}>`)
-      || msg.content.startsWith(`<@!${user().id}>`)
+    || msg.content.startsWith(`<@!${user().id}>`)
   ) {
     isMention = true;
   } else if (msg.content.startsWith(PREFIXES[msg.guild.id])) {
@@ -56,8 +103,8 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
     msg.delete();
     return;
   }
-  let command:string;
-  let content:string;
+  let command: string;
+  let content: string;
   if (isMention) {
     command = msg.content.split(' ')[1]; // eslint-disable-line prefer-destructuring
     content = msg.content
@@ -70,20 +117,20 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
       .substring(PREFIXES[msg.guild.id].length, msg.content.length)
       .split(' ')[0]
       .toLowerCase();
-    content = msg.content.slice(
-      command.length + PREFIXES[msg.guild.id].length,
-    ); // delete prefix and command
+    content = msg.content.slice(command.length + PREFIXES[msg.guild.id].length); // delete prefix and command
     content = msg.content.replace(/^\s+/g, ''); // delete leading spaces
   }
   if (command) {
-    let commandVal:string;
+    let commandVal: string;
     const pre = command.charAt(0);
-    const myPerms = (msg.channel as Discord.TextChannel).permissionsFor(msg.guild.me);
+    const myPerms = (msg.channel as Discord.TextChannel).permissionsFor(
+      msg.guild.me,
+    );
     if (pre === '.') {
       command = command.slice(1);
       commandVal = command;
     } else if (pre === ':') {
-      command = `@${command.slice(1)}`;
+      command = `_${command.slice(1)}`;
       commandVal = command.slice(1);
       // Check if its an admin command
       // if not you're allowed to use the normal version as admin (in any channel)
@@ -100,7 +147,8 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
             msg.delete();
           }
           if (myPerms.has('SEND_MESSAGES')) {
-            msg.reply("you're not allowed to use this command.")
+            msg
+              .reply("you're not allowed to use this command.")
               .then((mess) => (mess as Discord.Message).delete({ timeout: 5000 }));
           }
         }
@@ -112,17 +160,14 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
 
     if (
       commands[command]
-        && (!commands[command].dev || msg.author.id === OWNERID)
+      && (!commands[command].dev || msg.author.id === OWNERID)
     ) {
       if (
         pre === ':'
-          || (await data.commandAllowed(msg.guild.id, msg.channel.id))
+        || (await data.commandAllowed(msg.guild.id, msg.channel.id))
       ) {
         const perms = commands[command].perm;
-        if (
-          !perms
-            || (myPerms && myPerms.has(perms))
-        ) {
+        if (!perms || (myPerms && myPerms.has(perms))) {
           // cooldown for command usage
           if (!userCooldowns.has(msg.author.id)) {
             userCooldowns.add(msg.author.id);
@@ -140,24 +185,17 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
               );
             }
             data.usageUp(msg.author.id, msg.guild.id);
-          } else if (myPerms && myPerms.has('SEND_MESSAGES')
-          ) {
+          } else if (myPerms && myPerms.has('SEND_MESSAGES')) {
             msg.reply("whoa cool down, you're using commands too quick!");
           }
           // endof cooldown management
-        } else if (
-          myPerms && myPerms.has('SEND_MESSAGES')
-        ) {
+        } else if (myPerms && myPerms.has('SEND_MESSAGES')) {
           msg.channel.send(
             `I don't have the permissions needed for this command. (${perms}) `,
           );
         }
-      } else if (
-        myPerms && myPerms.has('SEND_MESSAGES')
-      ) {
-        if (
-          myPerms && myPerms.has('MANAGE_MESSAGES')
-        ) {
+      } else if (myPerms && myPerms.has('SEND_MESSAGES')) {
+        if (myPerms && myPerms.has('MANAGE_MESSAGES')) {
           msg.delete();
         }
         msg
@@ -176,4 +214,4 @@ export async function checkCommand(msg:Discord.Message, bot:Discord.Client) {
   }
 
   if (DELETE_COMMANDS) msg.delete();
-};
+}
