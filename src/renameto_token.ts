8e79c61320ea9ff363462c72705e9e5bb1b98618
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-06-24 18:14:16 +0200

    Delete renameto_token.ts

--- src/renameto_token.ts
+++
@@ -1,9 +0,0 @@
-// access to bot
-// rename this file to token.ts
-export default {
-  tk: 'discordbottoken',
-  owner: 'discordbotownerID',
-  prefix: 'a', // has to be single char only, otherwise the assumption is false for a lot of functions
-  dburl: 'databaseurl',
-  blapis: [], // botlist access token array (format as in BLAPI)
-};
