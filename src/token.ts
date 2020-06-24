704b9a98d15b9b0cbf7b59d5f764f5aecd2d1616
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-06-24 14:35:04 +0200

    allow no tokens for botlists at all

--- src/token.ts
+++ src/token.ts
@@ -6,10 +6,18 @@ const token = process.env.TOKEN;
 const owner = process.env.OWNERID;
 const prefix = process.env.PREFIX;
 const dburl = process.env.DATABASE_URL;
-// eslint-disable-next-line global-require
-const blapis = (require('./botlistTokens.json') || {}) as {
+let blapis: {
   [listname: string]: string;
 };
+
+try {
+  // eslint-disable-next-line global-require
+  blapis = require('./botlistTokens.json');
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.warn('no bot list tokens found, defaulting to none');
+  blapis = {};
+}
 
 if (!(token && owner && prefix && dburl)) {
   throw new Error('Missing .env configuration!');
