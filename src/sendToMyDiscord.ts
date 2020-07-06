f3f90919b9d1004dd06825c8a8ce62cfeaa12b66
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-07-06 10:47:57 +0200

    Update sendToMyDiscord.ts

--- src/sendToMyDiscord.ts
+++ src/sendToMyDiscord.ts
@@ -1,13 +1,22 @@
-﻿// commands made by Basti for use of the Bot
-import Discord from 'discord.js';
+﻿import Discord from 'discord.js';
+// eslint-disable-next-line import/no-cycle
 import { bot } from './bot';
+
+const maxMessageLength = 1950;
 
 export async function catchErrorOnDiscord(message: string) {
   try {
     const chann = await bot.channels.fetch('414809410448261132');
     if (chann) {
-      (chann as Discord.TextChannel).send(message.substring(0, 1950));
-      // TODO loop over the whole string length?
+      let idx = 0;
+      for (let i = 0; i < message.length; i += maxMessageLength) {
+        idx++;
+        // I want this to never hit ratelimit, so let's take it slow
+        // eslint-disable-next-line no-await-in-loop
+        await (chann as Discord.TextChannel).send(
+          `${idx}: ${message.substring(i, i + maxMessageLength)}`,
+        );
+      }
     }
   } catch (error) {
     console.error(error);
