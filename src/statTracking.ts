bdfd7ec95fcc5af2f675fee3383c6c6460e8e885
T0TProduction <sebidiluzio@gmail.com>
2020-08-29 11:01:51 +0200

    add custom stat tracking

---
+++ src/statTracking.ts
@@ -0,0 +1,25 @@
+// following functions are async because the statcord types expect promises
+
+let joinsoundsPlayed = 0;
+
+export async function sendJoinSoundsPlayed() {
+  const ret = joinsoundsPlayed;
+  joinsoundsPlayed = 0;
+  return String(ret);
+}
+
+export function playedJoinsound() {
+  joinsoundsPlayed++;
+}
+
+let usersWhoJoinedQueue = 0;
+
+export async function sendUsersWhoJoinedQueue() {
+  const ret = usersWhoJoinedQueue;
+  usersWhoJoinedQueue = 0;
+  return String(ret);
+}
+
+export function userJoinedQueue() {
+  usersWhoJoinedQueue++;
+}
