a82a7813ec9a6e824a1716441cef62d778dee47c
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-05-28 22:14:25 +0200

    fix collection naming and add logging (and deletion) of duplicate users that deny our indexing

--- src/db.ts
+++ src/db.ts
@@ -34,71 +34,77 @@ type Settings = {
   prefix: string;
   lastConnected: Date;
 };
-const settingsSchema = new mongoose.Schema<Settings, Model<Settings>>({
-  _id: {
-    // _id is auto indexed and unique
-    type: String,
-    required: true,
+const settingsSchema = new mongoose.Schema<Settings, Model<Settings>>(
+  {
+    _id: {
+      // _id is auto indexed and unique
+      type: String,
+      required: true,
+    },
+    commandChannels: {
+      type: [String],
+      required: true,
+    },
+    adminRoles: {
+      type: [String],
+      required: true,
+    },
+    joinChannels: {
+      type: [String],
+      required: true,
+    },
+    blacklistedUsers: {
+      type: [String],
+      required: true,
+    },
+    blacklistedEveryone: {
+      type: [String],
+      required: true,
+    },
+    saltKing: {
+      type: String,
+      required: false,
+    },
+    saltRole: {
+      type: String,
+      required: false,
+    },
+    notChannel: {
+      type: String,
+      required: false,
+    },
+    prefix: {
+      type: String,
+      required: true,
+    },
+    lastConnected: {
+      type: Date,
+      required: true,
+    },
   },
-  commandChannels: {
-    type: [String],
-    required: true,
-  },
-  adminRoles: {
-    type: [String],
-    required: true,
-  },
-  joinChannels: {
-    type: [String],
-    required: true,
-  },
-  blacklistedUsers: {
-    type: [String],
-    required: true,
-  },
-  blacklistedEveryone: {
-    type: [String],
-    required: true,
-  },
-  saltKing: {
-    type: String,
-    required: false,
-  },
-  saltRole: {
-    type: String,
-    required: false,
-  },
-  notChannel: {
-    type: String,
-    required: false,
-  },
-  prefix: {
-    type: String,
-    required: true,
-  },
-  lastConnected: {
-    type: Date,
-    required: true,
-  },
-});
+  { collection: 'settings' },
+);
 export const SettingsModel = mongoose.model('settings', settingsSchema);
 
 // saltrank per user
 type Saltrank = { salter: string; salt: number; guild: string };
-const saltrankSchema = new mongoose.Schema<Saltrank, Model<Saltrank>>({
-  salter: {
-    type: String,
-    required: true,
+const saltrankSchema = new mongoose.Schema<Saltrank, Model<Saltrank>>(
+  {
+    salter: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: Number,
+      required: true,
+    },
+    guild: {
+      type: String,
+      required: true,
+    },
   },
-  salt: {
-    type: Number,
-    required: true,
-  },
-  guild: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: 'saltrank' },
+);
 saltrankSchema.index({ salter: 1, guild: -1 }, { unique: true });
 export const SaltrankModel = mongoose.model('saltrank', saltrankSchema);
 
@@ -109,25 +115,28 @@ type Salt = {
   date: Date;
   guild: string;
 };
-const saltSchema = new mongoose.Schema<Salt, Model<Salt>>({
-  salter: {
-    type: String,
-    required: true,
+const saltSchema = new mongoose.Schema<Salt, Model<Salt>>(
+  {
+    salter: {
+      type: String,
+      required: true,
+    },
+    reporter: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    guild: {
+      type: String,
+      required: true,
+      index: true,
+    },
   },
-  reporter: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  guild: {
-    type: String,
-    required: true,
-    index: true,
-  },
-});
+  { collection: 'salt' },
+);
 saltSchema.index({ salter: 1, reporter: -1 });
 export const SaltModel = mongoose.model('salt', saltSchema);
 
@@ -141,38 +150,74 @@ type User = {
   botusage: number;
   sound?: string;
 };
-const userSchema = new mongoose.Schema<User, Model<User>>({
-  userID: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema<User, Model<User>>(
+  {
+    userID: {
+      type: String,
+      required: true,
+    },
+    guildID: {
+      type: String,
+      required: true,
+    },
+    warnings: {
+      type: Number,
+      required: true,
+    },
+    kicks: {
+      type: Number,
+      required: true,
+    },
+    bans: {
+      type: Number,
+      required: true,
+    },
+    botusage: {
+      type: Number,
+      required: true,
+    },
+    sound: {
+      type: String,
+      required: false,
+    },
   },
-  guildID: {
-    type: String,
-    required: true,
-  },
-  warnings: {
-    type: Number,
-    required: true,
-  },
-  kicks: {
-    type: Number,
-    required: true,
-  },
-  bans: {
-    type: Number,
-    required: true,
-  },
-  botusage: {
-    type: Number,
-    required: true,
-  },
-  sound: {
-    type: String,
-    required: false,
-  },
-});
+  { collection: 'users' },
+);
 userSchema.index({ userID: 1, guildID: -1 }, { unique: true });
-export const UserModel = mongoose.model('user', userSchema);
+export const UserModel = mongoose.model('users', userSchema);
+
+const deleteDuplicateUsers = false;
+// TODO check if we have duplicates on live
+UserModel.aggregate([
+  {
+    $group: {
+      _id: {
+        userID: '$userID',
+        guildID: '$guildID',
+      },
+      count: {
+        $sum: 1,
+      },
+    },
+  },
+]).then((duplicateUsers) => {
+  console.log(
+    'duplicate users:',
+    JSON.stringify(
+      duplicateUsers.filter((u) => u.count > 1),
+      null,
+      2,
+    ),
+  );
+  if (deleteDuplicateUsers) {
+    duplicateUsers
+      .filter((u) => u.count > 1)
+      .forEach(async (u) => {
+        await UserModel.deleteMany(u._id);
+        console.log('Deleted user', u._id);
+      });
+  }
+});
 
 // entry per vote
 type Vote = {
@@ -184,54 +229,60 @@ type Vote = {
   guildid: string;
   authorID: string;
 };
-const voteSchema = new mongoose.Schema<Vote, Model<Vote>>({
-  messageID: {
-    type: String,
-    required: true,
+const voteSchema = new mongoose.Schema<Vote, Model<Vote>>(
+  {
+    messageID: {
+      type: String,
+      required: true,
+    },
+    channelID: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: [String],
+      required: true,
+    },
+    topic: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    guildid: {
+      type: String,
+      required: true,
+    },
+    authorID: {
+      type: String,
+      required: true,
+    },
   },
-  channelID: {
-    type: String,
-    required: true,
-  },
-  options: {
-    type: [String],
-    required: true,
-  },
-  topic: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  guildid: {
-    type: String,
-    required: true,
-  },
-  authorID: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: 'votes' },
+);
 voteSchema.index({ messageID: 1, channelID: 1 });
-export const VoteModel = mongoose.model('vote', voteSchema);
+export const VoteModel = mongoose.model('votes', voteSchema);
 
 // one entry per user per guild
 // deprecated, we want to remove this system?
 type StillMuted = { userid: string; guildid: string };
-const stillMutedSchema = new mongoose.Schema<StillMuted, Model<StillMuted>>({
-  userid: {
-    type: String,
-    required: true,
+const stillMutedSchema = new mongoose.Schema<StillMuted, Model<StillMuted>>(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    guildid: {
+      type: String,
+      required: true,
+    },
   },
-  guildid: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: 'stillMuted' },
+);
 stillMutedSchema.index({ userid: 1, guildid: 1 });
-export const StillMutedModel = mongoose.model('stillmuted', stillMutedSchema);
+export const StillMutedModel = mongoose.model('stillMuted', stillMutedSchema);
 
 // Define Methods, these are reworked versions of the old ones and will stay:
 async function getuser(userid: string, guildID: string) {
