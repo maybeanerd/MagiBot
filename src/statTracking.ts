bfcf220041501f7b60987fd415c61efe7c8ddc73
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-06-06 22:50:24 +0200

    catch erofs errors for heroku hosted bot

--- src/statTracking.ts
+++ src/statTracking.ts
@@ -1,29 +1,37 @@
 // following functions are async because the statcord types expect promises
 import { ShardingManager } from 'discord.js';
 import { promises as fsp } from 'fs';
-import { asyncForEach, returnNullOnError } from './bamands';
+import { asyncForEach, doNothingOnError, returnNullOnError } from './bamands';
 
 // attempt to share data over saved files; unfinished and therefore not used
 
 export async function saveJoinsoundsPlayedOfShard(shardId: number) {
-	const data = await fsp
-		.readFile(`/shard-${shardId}-jsplayed`, 'utf8')
-		.catch(returnNullOnError);
-	let joinsoundsPlayed = data ? Number(data) : 0;
-	return fsp.writeFile(
-		`/shard-${shardId}-jsplayed`,
-		String(++joinsoundsPlayed),
-	);
+	try {
+		const data = await fsp
+			.readFile(`/shard-${shardId}-jsplayed`, 'utf8')
+			.catch(returnNullOnError);
+		let joinsoundsPlayed = data ? Number(data) : 0;
+		await fsp.writeFile(
+			`/shard-${shardId}-jsplayed`,
+			String(++joinsoundsPlayed),
+		);
+	} catch (e) {
+		doNothingOnError();
+	}
 }
 export async function saveUsersWhoJoinedQueue(shardId: number) {
-	const data = await fsp
-		.readFile(`/shard-${shardId}-uqueue`, 'utf8')
-		.catch(returnNullOnError);
-	let usersWhoJoinedQueue = data ? Number(data) : 0;
-	return fsp.writeFile(
-		`/shard-${shardId}-uqueue`,
-		String(++usersWhoJoinedQueue),
-	);
+	try {
+		const data = await fsp
+			.readFile(`/shard-${shardId}-uqueue`, 'utf8')
+			.catch(returnNullOnError);
+		let usersWhoJoinedQueue = data ? Number(data) : 0;
+		await fsp.writeFile(
+			`/shard-${shardId}-uqueue`,
+			String(++usersWhoJoinedQueue),
+		);
+	} catch (e) {
+		doNothingOnError();
+	}
 }
 async function loadJoinsoundsPlayedOfShard(
 	shardId: number,
@@ -59,7 +67,7 @@ export async function accumulateJoinsoundsPlayed(manager: ShardingManager) {
 	dataMap.forEach((val) => {
 		amount += val;
 	});
-	console.log('joinsounds played:', amount);
+	// console.log('joinsounds played:', amount);
 	return String(amount);
 }
 export async function accumulateUsersJoinedQueue(manager: ShardingManager) {
@@ -71,6 +79,6 @@ export async function accumulateUsersJoinedQueue(manager: ShardingManager) {
 	dataMap.forEach((val) => {
 		amount += val;
 	});
-	console.log('users joined:', amount);
+	// console.log('users joined:', amount);
 	return String(amount);
 }
