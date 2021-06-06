dba780e26f93db674070e7569cc1677e725522ea
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-06-06 21:25:41 +0200

    add "debug logs" to see if we actually post the stats to statcord, or not (im assuming not)

--- src/statTracking.ts
+++ src/statTracking.ts
@@ -5,11 +5,16 @@ let joinsoundsPlayed = 0;
 export async function sendJoinSoundsPlayed() {
 	const ret = joinsoundsPlayed;
 	joinsoundsPlayed = 0;
+	console.log('[Statcord custom stats] Sending joinsounds played:', ret);
 	return String(ret);
 }
 
 export function playedJoinsound() {
 	joinsoundsPlayed++;
+	console.log(
+		'[Statcord custom stats] Tracking joinsounds played:',
+		joinsoundsPlayed,
+	);
 }
 
 let usersWhoJoinedQueue = 0;
@@ -17,9 +22,14 @@ let usersWhoJoinedQueue = 0;
 export async function sendUsersWhoJoinedQueue() {
 	const ret = usersWhoJoinedQueue;
 	usersWhoJoinedQueue = 0;
+	console.log('[Statcord custom stats] Sending users joined queue:', ret);
 	return String(ret);
 }
 
 export function userJoinedQueue() {
 	usersWhoJoinedQueue++;
+	console.log(
+		'[Statcord custom stats] Tracking users joined queue:',
+		usersWhoJoinedQueue,
+	);
 }
