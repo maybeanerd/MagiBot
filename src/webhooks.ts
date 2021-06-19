9bb13ff2be529b19da08a10021afa72e663b8851
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-06-19 12:15:35 +0200

    Add discord webhooks (#150)
    * remove unused deps
    
    * first attempt at replacing normal messages with webhooks for things that are supposed to got to teabots
    
    * remove queue start message as well
    
    * Update rename-me.env

---
+++ src/webhooks.ts
@@ -0,0 +1,66 @@
+import Discord from 'discord.js';
+import { doNothingOnError } from './bamands';
+
+const {
+	WEBHOOK_ID_EX,
+	WEBHOOK_TOKEN_EX,
+	WEBHOOK_ID_JOIN,
+	WEBHOOK_TOKEN_JOIN,
+	WEBHOOK_ID_BUG,
+	WEBHOOK_TOKEN_BUG,
+	WEBHOOK_ID_STARTUP,
+	WEBHOOK_TOKEN_STARTUP,
+} = process.env;
+if (
+	!(
+		WEBHOOK_ID_EX
+    && WEBHOOK_TOKEN_EX
+    && WEBHOOK_ID_JOIN
+    && WEBHOOK_TOKEN_JOIN
+    && WEBHOOK_ID_BUG
+    && WEBHOOK_TOKEN_BUG
+    && WEBHOOK_ID_STARTUP
+    && WEBHOOK_TOKEN_STARTUP
+	)
+) {
+	throw new Error('Discord Webhook information is missing.');
+}
+
+const exceptionsWebhook = new Discord.WebhookClient(
+	WEBHOOK_ID_EX,
+	WEBHOOK_TOKEN_EX,
+);
+const joinsWebhook = new Discord.WebhookClient(
+	WEBHOOK_ID_JOIN,
+	WEBHOOK_TOKEN_JOIN,
+);
+const bugreportWebhook = new Discord.WebhookClient(
+	WEBHOOK_ID_BUG,
+	WEBHOOK_TOKEN_BUG,
+);
+const startupWebhook = new Discord.WebhookClient(
+	WEBHOOK_ID_STARTUP,
+	WEBHOOK_TOKEN_STARTUP,
+);
+
+export async function sendException(value: string) {
+	return exceptionsWebhook.send(value).catch(doNothingOnError);
+}
+export async function sendJoinEvent(value: string) {
+	return joinsWebhook.send(value).catch(doNothingOnError);
+}
+export async function sendBugreport(value: string) {
+	return bugreportWebhook.send(value).catch(doNothingOnError);
+}
+export async function sendStartupEvent(
+	shardId: number,
+	justStartingUp = false,
+) {
+	return startupWebhook
+		.send(
+			justStartingUp
+				? `Shard ${shardId} is starting...!`
+				: `Shard ${shardId} is up!`,
+		)
+		.catch(doNothingOnError);
+}
