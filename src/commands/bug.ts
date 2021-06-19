32e67aac1526f0cd695b639b11ee7d9f6de9211a
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-06-19 12:15:35 +0200

    Add discord webhooks (#150)
    * remove unused deps
    
    * first attempt at replacing normal messages with webhooks for things that are supposed to got to teabots
    
    * remove queue start message as well
    
    * Update rename-me.env

--- src/commands/bug.ts
+++ src/commands/bug.ts
@@ -4,15 +4,16 @@ import { bot } from '../bot';
 import { PREFIXES } from '../shared_assets';
 import { commandCategories } from '../types/enums';
 import { magibotCommand } from '../types/magibot';
+import { sendBugreport } from '../webhooks';
 
 export const bug: magibotCommand = {
 	name: 'bug',
 	async main(content, msg) {
 		if (!(content.length > 0)) {
 			msg.reply(
-				`you need to add info about the report after the command. Use \`${
-					PREFIXES.get(msg.guild!.id)
-				}.help bug\` to get more info.`,
+				`you need to add info about the report after the command. Use \`${PREFIXES.get(
+          msg.guild!.id,
+				)}.help bug\` to get more info.`,
 			);
 			return;
 		}
@@ -20,7 +21,7 @@ export const bug: magibotCommand = {
 			.send(`Do you want to send this bugreport?\n${content}`)
 			.then((mess) => {
 				const filter = (reaction, user) => (reaction.emoji.name === '☑' || reaction.emoji.name === '❌')
-					&& user.id === msg.author.id;
+          && user.id === msg.author.id;
 				mess.react('☑');
 				mess.react('❌');
 				mess
@@ -32,18 +33,15 @@ export const bug: magibotCommand = {
 						mess.delete();
 						const frst = reacts.first();
 						if (frst && frst.emoji.name === '☑') {
-							const chann = await bot.channels.fetch('444529194949672972');
-							(chann as TextChannel)
-								.send(
-									`**Bugreport** by ${msg.author.username} (<@${
-										msg.author.id
-									}>) on server ${msg.guild!.name}( ${msg.guild!.id} ) :\n${
-										content
-									}`,
-								)
-								.then(() => {
-									msg.channel.send('Succesfully sent bugreport.');
-								});
+							sendBugreport(
+								`**Bugreport** by ${msg.author.username} (<@${
+									msg.author.id
+								}>) on server ${msg.guild!.name}( ${
+                  msg.guild!.id
+								} ) :\n${content}`,
+							).then(() => {
+								msg.channel.send('Succesfully sent bugreport.');
+							});
 						} else if (reacts.first()) {
 							msg.channel.send('Successfully canceled bugreport.');
 						}
@@ -54,8 +52,7 @@ export const bug: magibotCommand = {
 	ehelp() {
 		return [
 			{
-				name:
-					'<bugreport with information about what you did, what was expected, and what went wrong>',
+				name: '<bugreport with information about what you did, what was expected, and what went wrong>',
 				value: 'Report a bug concerning MagiBot',
 			},
 		];
