7f4a21200305554300c5ace53985d8a5218e59c2
Sebastian Di Luzio <sebidiluzio@gmail.com>
2022-02-22 20:56:19 +0100

    adjust interactionConfirmation, bugreport

--- src/commands/bug.ts
+++ src/commands/bug.ts
@@ -1,36 +1,43 @@
 ﻿import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
-import { interactionConfirmation, notifyAboutSlashCommand } from '../helperFunctions';
+import {
+	interactionConfirmation,
+	notifyAboutSlashCommand,
+} from '../helperFunctions';
 import { commandCategories } from '../types/enums';
 import { magibotCommand } from '../types/magibot';
 import { sendBugreport } from '../webhooks';
 
 const slashCommand = new SlashCommandBuilder()
 	.setName('bugreport')
-	.setDescription('Report a bug concerning MagiBot').addStringOption(
-		(option) => option
-			.setName('description')
-			.setDescription(
-				'Describe what you did, what was expected, and what went wrong',
-			)
-			.setRequired(true),
-	);
+	.setDescription('Report a bug concerning MagiBot')
+	.addStringOption((option) => option
+		.setName('description')
+		.setDescription(
+			'Describe what you did, what was expected, and what went wrong',
+		)
+		.setRequired(true));
+
 async function main(interaction: CommandInteraction, input: string) {
 	const confirmed = await interactionConfirmation(
 		interaction,
 		`Do you want to send this bugreport?\n${input}`,
-		'Successfully canceled bugreport.',
+		'Successfully cancelled bugreport.',
 	);
 	if (confirmed) {
 		await sendBugreport(
-			`**Bugreport** by ${interaction.member?.user.username
-			} (<@${
+			`**Bugreport** by ${interaction.member?.user.username} (<@${
 				interaction.member?.user.id
-			}>) on server ${interaction.guild!.name}( ${interaction.guild!.id} ) :\n${input}`,
+			}>) on server ${interaction.guild!.name}( ${
+        interaction.guild!.id
+			} ) :\n${input}`,
 		);
-		await interaction.followUp('Succesfully sent bugreport.');
+		await confirmed.reply({
+			content: `Successfully sent bugreport:\n${input}`,
+		});
 	}
 }
+
 export const bugreport: magibotCommand = {
 	name: 'bug',
 	async main({ message }) {
