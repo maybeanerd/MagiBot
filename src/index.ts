336d43d80358c32483b671cced09e78d8a4021aa
Sebastian Di Luzio <sebastian@diluz.io>
2026-04-03 21:47:00 +0200

    fix: remove statcord references

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { ShardingManager } from 'discord.js';
+import { ShardingManager } from "discord.js";
 /* import Statcord from 'statcord.js'; */
-import configuration from './configuration';
-import { syncGlobalCommands } from './commandSync';
+import configuration from "./configuration";
+import { syncGlobalCommands } from "./commandSync";
 /* import {
 accumulateJoinsoundsPlayed,
 accumulateUsersJoinedQueue,
@@ -9,18 +9,9 @@ accumulateUsersJoinedQueue,
 
 export const TOKEN = configuration.tk;
 
-const manager = new ShardingManager('dist/bot.js', {
+const manager = new ShardingManager("dist/bot.js", {
   token: TOKEN,
 });
-
-if (!process.env.STATCORD_TOKEN) {
-  throw new Error('Statcord token missing!');
-}
-
-/* export const statcord = new Statcord.ShardingClient({
-  manager,
-  key: process.env.STATCORD_TOKEN,
-}); */
 
 // register custom stats
 // statcord.registerCustomFieldHandler(1, accumulateJoinsoundsPlayed);
@@ -31,7 +22,7 @@ if (!process.env.STATCORD_TOKEN) {
   console.log('[Statcord]: Started autopost');
 }); */
 
-manager.on('shardCreate', (shard) => console.log(`Launched shard ${shard.id}`));
+manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
 
 manager.spawn();
 
